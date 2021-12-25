b461cca6a96078da00ecbf683f3bcc2eff0e9405
Dino <elvedingran@gmail.com>
2021-12-25 20:03:20 +0100

    Added Controller

---
+++ RS1-Seminarski/Controllers/TrackController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RS1_Seminarski.Data;
+using RS1_Seminarski.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Seminarski.Controllers
+{
+    public class TrackController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TrackController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Track> objList = _db.Track;
+            return View(objList); 
+        }
+    }
+}
