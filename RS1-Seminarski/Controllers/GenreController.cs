5dea448996c4c616fac36ba023abf69d3dc77473
Dino <elvedingran@gmail.com>
2021-12-26 17:53:43 +0100

    Added genres

---
+++ RS1-Seminarski/Controllers/GenreController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RS1_Seminarski.Data;
+using RS1_Seminarski.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Seminarski.Controllers
+{
+    public class GenreController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public GenreController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Genre> obj = _db.Genre;
+            return View(obj);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Genre obj)
+        {
+            _db.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
