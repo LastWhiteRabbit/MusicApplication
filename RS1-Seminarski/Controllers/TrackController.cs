301e2c5bd751271f1fd7f44b8c2dd98d1b377886
Dino <elvedingran@gmail.com>
2021-12-27 20:30:10 +0100

    Added update

--- RS1-Seminarski/Controllers/TrackController.cs
+++ RS1-Seminarski/Controllers/TrackController.cs
@@ -66,5 +66,33 @@ namespace RS1_Seminarski.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Track.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Track obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Track.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
